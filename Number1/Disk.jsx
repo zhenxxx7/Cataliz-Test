de6ffd4809c24855d37bc0866b26927ac79ed425
zhenxxx7 <jordy.zeventh@gmail.com>
2023-02-07 19:48:53 +0700

    add Number1

---
+++ Number1/Disk.jsx
@@ -0,0 +1,11 @@
+function towerOfHanoi(n, from, to, aux) {
+  if (n === 1) {
+    console.log(`Move disk 1 from ${from} to ${to}`);
+    return;
+  }
+  towerOfHanoi(n - 1, from, aux, to);
+  console.log(`Move disk ${n} from ${from} to ${to}`);
+  towerOfHanoi(n - 1, aux, to, from);
+}
+
+towerOfHanoi(3, 'A', 'C', 'B');
